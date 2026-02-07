2da4266c487c9e843d3dc310d62534de17c62257
andreasgriffin <andreasgriffin@proton.me>
2026-02-07 10:15:42 +0100

    upgrade wine

--- tools/deterministic-build/compile.sh
+++ tools/deterministic-build/compile.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 SCRIPT_DIR="tools/deterministic-build"
 
+pip install --upgrade pip-tools
 rm "${SCRIPT_DIR}/requirements-build.txt" || true
 pip-compile "${SCRIPT_DIR}/requirements-build.in" --generate-hashes --allow-unsafe
